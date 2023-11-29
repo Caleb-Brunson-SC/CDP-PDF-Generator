3017d31d4fe4d0aff4acfd83f84794cd0bf364b0
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-11-29 16:46:50 -0500

    Removed code from JavaFx install docs

--- pdfGenerator/src/cdpfx/Main.java
+++ pdfGenerator/src/cdpfx/Main.java
@@ -1,19 +1,22 @@
 package cdpfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
+  private Parent createContent() {
+    return new StackPane(new Text("Hello World"));
+  }
+
   @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("cdpfx.fxml"));
-    primaryStage.setTitle("CDP Fencing PDF Generator");
-    primaryStage.setScene(new Scene(root, 700, 500));
-    primaryStage.show();
+  public void start(Stage stage) throws Exception {
+    stage.setScene(new Scene(createContent(), 300, 300));
+    stage.show();
   }
 
   public static void main(String[] args) {
