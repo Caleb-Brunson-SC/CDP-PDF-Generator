a33b22b1b7ffcd4502687b23ed216c69f7889480
Caleb-Brunson-SC <caleb.brunson.professional@gmail.com>
2023-11-30 15:48:43 -0500

    Added PDFGenerator class with data attributes

---
+++ pdfGenerator/src/cdpfx/PDFGenerator.java
@@ -0,0 +1,23 @@
+package cdpfx;
+
+public class PDFGenerator {
+  public String proposal_creation_date;
+  public String customer_first_name;
+  public String customer_last_name;
+  public String service_address;
+  public String service_address_city_state_zip;
+  public String service_address_municipality;
+  public String customer_email_address;
+  public Boolean is_permit_required;
+  public Boolean is_homeowner_to_obtain_permit;
+  public Boolean is_cdp_fencing_to_obtain_permit;
+  public Boolean is_plan_plot_survey_available;
+  public String product_specifications;
+  public String job_options;
+  public String job_notes;
+  public String contract_amount;
+  public String sales_tax;
+  public String deposit_amount;
+  public String remaining_balance;
+  public String cdp_authorized_representative_name;
+}
